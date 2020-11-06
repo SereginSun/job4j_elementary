e880ab4545c60d7499c02bd88ae39d23f17be0f1
SereginSun <SereginSun@yandex.ru>
2020-11-06 19:08:19 +0300

    The program that prints the multiplication table to the console.

---
+++ job4j/src/ru/job4j/Multiply.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiply {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("%s x %s = %s%n",i, j, i * j);
+            }
+            System.out.printf("%n");
+        }
+    }
+}
