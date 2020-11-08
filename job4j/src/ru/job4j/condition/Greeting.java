3b2743ce6c953d2bee507c6ecefd7e8896a1a1ed
SereginSun <sereginsun@yandex.ru>
2020-11-08 21:33:11 +0300

    Greeting.

---
+++ job4j/src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like Java";
+        String separate = ", ";
+        String but = "but I'm a newbie";
+        int year = 2020;
+        System.out.printf("%s%s%s. %s.", idea, separate, but, year);
+    }
+}
