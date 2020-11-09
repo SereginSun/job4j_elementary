fe8089173af46fc21c73914f22bfce843c4ad98d
SereginSun <sereginsun@yandex.ru>
2020-11-09 09:18:18 +0300

    Changed the method Main as a unit test.

--- job4j/src/ru/job4j/converter/Converter.java
+++ job4j/src/ru/job4j/converter/Converter.java
@@ -13,9 +13,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        Converter conv = new Converter();
-        int value = 210;
-        System.out.printf("%s rubles are %s euro%n", value, conv.rubleToEuro(value));
-        System.out.printf("%s rubles are %s dollars", value, conv.rubleToDollar(value));
+        Converter conv1 = new Converter();
+        int value1 = 210;
+        int expected1 = 3;
+        int out1 = conv1.rubleToEuro(value1);
+        boolean passed1 = expected1 == out1;
+        System.out.printf("%s rubles are %s euro. Test result: %s%n", value1, conv1.rubleToEuro(value1), passed1);
+        Converter conv2 = new Converter();
+        int value2 = 240;
+        int expected2 = 4;
+        int out2 = conv2.rubleToDollar(value2);
+        boolean passed2 = expected2 == out2;
+        System.out.printf("%s rubles are %s dollars. Test result: %s.", value2, conv2.rubleToDollar(value2), passed2);
     }
 }
