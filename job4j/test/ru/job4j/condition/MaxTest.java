88695cbeb114b1346c859cdb0a4629f4cdead11a
SereginSun <sereginsun@yandex.ru>
2020-11-11 09:51:04 +0300

    Ternary comparison.

---
+++ job4j/test/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax5To2Then5() {
+        int result = Max.max(5, 2);
+        assertThat(result, is(5));
+    }
+
+    @Test
+    public void whenMax5To5Then5() {
+        int result = Max.max(5, 5);
+        assertThat(result, is(5));
+    }
+}
