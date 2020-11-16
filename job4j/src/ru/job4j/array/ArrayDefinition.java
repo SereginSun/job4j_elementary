c115ee8ec7aa292a37aac94e998b5c7fc1cf0fd9
SereginSun <sereginsun@yandex.ru>
2020-11-16 09:05:08 +0300

    Array size.

--- job4j/src/ru/job4j/array/ArrayDefinition.java
+++ job4j/src/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.printf("Array size of ages[]: %s%n", ages.length);
+        System.out.printf("Array size of surnames[]: %s%n", surnames.length);
+        System.out.printf("Array size of prices[]: %s%n", prices.length);
     }
 }
