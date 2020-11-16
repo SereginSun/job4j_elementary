a71690411806aab9b78391348dac041228a46f99
SereginSun <sereginsun@yandex.ru>
2020-11-16 09:13:53 +0300

    Filling the array.

--- job4j/src/ru/job4j/array/ArrayDefinition.java
+++ job4j/src/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.printf("Array size of ages[]: %s%n", ages.length);
         System.out.printf("Array size of surnames[]: %s%n", surnames.length);
         System.out.printf("Array size of prices[]: %s%n", prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Vladimir";
+        names[2] = "Oleg";
+        names[3] = "Svetlana";
+
+        for (var name: names) {
+            System.out.printf("%s%n", name);
+        }
     }
 }
