c005e60f75dc2b7af31b0b0d1f5f243398d27d6e
SereginSun <sereginsun@yandex.ru>
2020-11-25 10:48:24 +0300

    Mono row in matrix.

---
+++ job4j/test/ru/job4j/array/MatrixCheckTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MatrixCheckTest {
+
+    @Test
+    public void whenHasMonoHorizontal() {
+        MatrixCheck matrix = new MatrixCheck();
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
+        };
+        boolean result = matrix.monoHorizontal(input, 1);
+        assertTrue(result);
+    }
+
+    @Test
+    public void whenDontHasMonoHorizontal() {
+        MatrixCheck matrix = new MatrixCheck();
+        char[][] input = {
+                {' ', ' ', ' '},
+                {'X', 'X', ' '},
+                {' ', ' ', ' '},
+        };
+        boolean result = matrix.monoHorizontal(input, 1);
+        assertFalse(result);
+    }
+}
