e1158393ff190d3fd8cc8ffb0b241f182389bebe
SereginSun <sereginsun@yandex.ru>
2020-11-25 11:05:34 +0300

    An array from the matrix diagonal.

--- job4j/test/ru/job4j/array/MatrixCheckTest.java
+++ job4j/test/ru/job4j/array/MatrixCheckTest.java
@@ -2,8 +2,9 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class MatrixCheckTest {
 
@@ -53,5 +54,31 @@ public class MatrixCheckTest {
         };
         boolean result = matrix.monoVertical(input, 0);
         assertFalse(result);
+    }
+
+    @Test
+    public void whenMatrixHasMonoDiagonal() {
+        MatrixCheck matrix = new MatrixCheck();
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        char[] result = matrix.extractDiagonal(input);
+        char[] expect = {'X', 'X', 'X'};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenMatrixHasNonMonoDiagonal() {
+        MatrixCheck matrix = new MatrixCheck();
+        char[][] input = {
+                {'X', ' ', ' '},
+                {' ', 'X', ' '},
+                {' ', ' ', ' '},
+        };
+        char[] result = matrix.extractDiagonal(input);
+        char[] expect = {'X', 'X', ' '};
+        assertThat(result, is(expect));
     }
 }
