8a9295655a3d1a97e0dd343c8c9b0b7e7f076c3a
SereginSun <sereginsun@yandex.ru>
2020-12-08 10:00:06 +0300

    Refactoring the Max class. Method overloading max.

--- job4j/test/ru/job4j/condition/MaxTest.java
+++ job4j/test/ru/job4j/condition/MaxTest.java
@@ -24,4 +24,28 @@ public class MaxTest {
         int result = Max.max(5, 5);
         assertThat(result, is(5));
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int result = Max.max(1, 3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax7To5To3Then7() {
+        int result = Max.max(7, 5, 3);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenMax3To8To5To6Then8() {
+        int result = Max.max(3, 8, 5, 6);
+        assertThat(result, is(8));
+    }
+
+    @Test
+    public void whenMax1To2To3To4Then4() {
+        int result = Max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
 }
